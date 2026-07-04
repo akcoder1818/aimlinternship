aa7fca8fc04f4691b7d36a51f90f9a1ea4d8f109
abhishekkumarcoder <shuklaabhishek1832006@gmail.com>
2026-07-04 12:03:30 +0530

    Added internship project files

---
+++ test.py
@@ -0,0 +1,9 @@
+print("hii")
+import numpy as np
+print(np.__version__)
+import sklearn
+print(sklearn.__version__)
+import pandas
+print(pandas.__version__)
+import matplotlib
+print(matplotlib.__version__)
